0ef7ecb26bf8dcf46a5839c1024552e6100ea188
1658618651 <1658618651@qq.com>
2019-12-09 21:40:23 +0800

    header开发

---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="header">
+  <div class="header-left ">
+   <span class="iconfont back-icon">&#xe624;</span>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xe632;</span>
+   输入城市/景点，游玩/主题
+  </div>
+  <div class="header-right">
+    <span class="iconfont arrow-icon">&#xe64a;</span>
+   城市
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader"
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import './header.styl';
+</style>
